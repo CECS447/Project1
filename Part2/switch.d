88e1cc0c6df2d7fb0d382c8a2aba5424342f6fee
T3chnopath <josias.navasca@gmail.com>
2023-09-17 17:18:38 -0700

    Add part 2 starter

--- Part2/switch.d
+++
@@ -1,2 +0,0 @@
-./switch.o: switch.c tm4c123gh6pm.h switch.h music.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
--- Part2/systick.d
+++
@@ -1,2 +0,0 @@
-./systick.o: SysTick.c tm4c123gh6pm.h \
-  C:\Keil_v5\ARM\ARMCLANG\Bin\..\include\stdint.h
