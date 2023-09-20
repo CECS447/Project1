3f5811189f3c7f0c864cc0c33b8599e52ac223eb
T3chnopath <josias.navasca@gmail.com>
2023-09-20 00:33:50 -0700

    Repeat current song for part 2

--- Part2/Sound.c
+++ Part2/Sound.c
@@ -34,38 +34,39 @@ const uint32_t tonetab[] =
 // 1, 2, 3, 4, 5, 6, 7
 // lower C octave:130.813, 146.832,164.814,174.614,195.998, 220,246.942
 // calculate reload value for the whole period:Reload value = Fclk/Ft = 16MHz/Ft
-{122137,108844,96970,91429,81633,72727,64777,            // C3 Major Notes
+{
  30534*2,27211*2,24242*2,22923*2,20408*2,18182*2,16194*2, // C4 Major notes
  15289*2,13621*2,12135*2,11454*2,10204*2,9091*2,8099*2,   // C5 Major notes
- 7645*2,6810*2,6067*2,5727*2,5102*2,4545*2,4050*2};        // C6 Major notes
+ 7645*2,6810*2,6067*2,5727*2,5102*2,4545*2,4050*2
+};        // C6 Major notes
 
 // Constants
 // Index for notes used in the music scores
 typedef enum
 {
-  C3 = 0,
-  D3 = 1,
-  E3 = 2,
-  F3 = 3,
-  G3 = 4,
-  A3 = 5,
-  B3 = 6,
+  C4 = 0,
+  D4 = 1,
+  E4 = 2,
+  F4 = 3,
+  G4 = 4,
+  A4 = 5,
+  B4 = 6,
 
-  C4 = 0+7,
-  D4 = 1+7,
-  E4 = 2+7,
-  F4 = 3+7,
-  G4 = 4+7,
-  A4 = 5+7,
-  B4 = 6+7,
+  C5 = 0+7,
+  D5 = 1+7,
+  E5 = 2+7,
+  F5 = 3+7,
+  G5 = 4+7,
+  A5 = 5+7,
+  B5 = 6+7,
 
-  C5 = 0+2*7,
-  D5 = 1+2*7,
-  E5 = 2+2*7,
-  F5 = 3+2*7,
-  G5 = 4+2*7,
-  A5 = 5+2*7,
-  B5 = 6+2*7,
+  C6 = 0+2*7,
+  D6 = 1+2*7,
+  E6 = 2+2*7,
+  F6 = 3+2*7,
+  G6 = 4+2*7,
+  A6 = 5+2*7,
+  B6 = 6+2*7,
 
   PAUSE = 255,
 } NOTE_INDEX;
@@ -220,22 +221,22 @@ void GPIOPortD_Handler(void){
 
   if (GPIO_PORTD_RIS_R & KEY_C_MASK)
   {
-    Note = C3;
+    Note = C4;
     GPIO_PORTD_ICR_R |= KEY_C_MASK; // Ack interrupt 
   }
   else if (GPIO_PORTD_RIS_R & KEY_D_MASK )
   {
-    Note = D3;
+    Note = D4;
     GPIO_PORTD_ICR_R |= KEY_D_MASK; // Ack interrupt 
   }
   else if (GPIO_PORTD_RIS_R & KEY_E_MASK)
   {
-    Note = E3;
+    Note = E4;
     GPIO_PORTD_RIS_R |= KEY_E_MASK; // Ack interrupt 
   }
   else if (GPIO_PORTD_RIS_R & KEY_F_MASK)
   {
-    Note = F3;
+    Note = F4;
     GPIO_PORTD_ICR_R |= KEY_F_MASK; // Ack interrupt 
   }
 
@@ -302,6 +303,7 @@ void play_a_song()
 {
   uint8_t currentToneIndex = 0;
   uint8_t currentDelay = getDelay();
+  DelayMS(100); // Delay between subsequent plays
 	while ( currentDelay && curr_mode == AUTO_PLAY )
   {
     currentToneIndex = getToneIndex();
@@ -327,8 +329,6 @@ void play_a_song()
     currentDelay = getDelay();
   }
   curr_note = 0;
-  curr_song = (curr_song + 1) % 3;
-  DelayMS(100); //Delay between subsequent songs
 }
 
 
